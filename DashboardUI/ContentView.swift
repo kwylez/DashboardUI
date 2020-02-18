5e8843d061fe84b5003a9a1a7141bf00b2dd60a1
Cory D. Wiles <corywiles@icloud.com>
2020-02-18 10:51:20 -0500

    Initial Commit

---
+++ DashboardUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DashboardUI
+//
+//  Created by Cory D. Wiles on 2/18/20.
+//  Copyright © 2020 Cory D. Wiles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
