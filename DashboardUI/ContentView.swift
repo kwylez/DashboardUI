01939b8a20e48cd711262c923aca126d4617e2dc
Cory D. Wiles <corywiles@icloud.com>
2020-02-18 22:30:32 -0500

    basic grid for dashboard elements

--- DashboardUI/ContentView.swift
+++ DashboardUI/ContentView.swift
@@ -10,7 +10,98 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack {
+            LinearGradient(
+                gradient:
+                Gradient(
+                    colors: [
+                        Color("TopGradient"),
+                        Color("BottomGradient")
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            ).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Image("Profile")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.8), radius: 10.0)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                        .padding(.leading)
+                    Spacer()
+                }
+                HStack(alignment: .top) {
+                    VStack(spacing: 50.0) {
+                        Image(systemName: "plus")
+                            .font(.system(.largeTitle))
+                        Image(systemName: "plus")
+                            .font(.system(.largeTitle))
+                        Image(systemName: "plus")
+                            .font(.system(.largeTitle))
+                        Image(systemName: "plus")
+                            .font(.system(.largeTitle))
+                    }
+                    .padding(.trailing, 70.0)
+                    .padding(.top, 90.0)
+                    VStack(alignment: .leading) {
+                        Text("Dashboard")
+                            .font(.system(.largeTitle)).bold()
+                            .foregroundColor(.white)
+                            .padding(.bottom)
+                        HStack(spacing: 20.0) {
+                            Text("Sales")
+                                .foregroundColor(.white)
+                                .font(.system(.body)).bold()
+                            Text("Earnings")
+                                .foregroundColor(.white)
+                                .font(.system(.body)).bold()
+                            Text("Revenue")
+                                .foregroundColor(.white)
+                                .font(.system(.body)).bold()
+                        }
+                        .padding(.bottom, 50.0)
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .foregroundColor(.white)
+                            .frame(width: 400.0, height: 300.0)
+                        Text("Stats")
+                            .foregroundColor(.white)
+                            .font(.system(.body)).bold()
+                            .padding(.bottom)
+                            .padding(.top, 45.0)
+                        HStack(spacing: 100.0) {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Total Earnings")
+                                    .font(.system(.caption))
+                                Text("$1245")
+                                    .font(.system(.body)).bold()
+                            }
+                            .foregroundColor(.white)
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Product Sold")
+                                    .font(.system(.caption))
+                                Text("899")
+                                    .font(.system(.body)).bold()
+                            }
+                            .foregroundColor(.white)
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Ratings")
+                                    .font(.system(.caption))
+                                Text("73%")
+                                    .font(.system(.body)).bold()
+                            }
+                            .foregroundColor(.white)
+                        }
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding(.top)
+        }
     }
 }
 
