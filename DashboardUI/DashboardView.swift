1a90f2b3d8fb0a75947703c09313a3ce2879a5c3
Cory D. Wiles <corywiles@icloud.com>
2020-02-24 09:00:11 -0500

    use lineview for display

--- DashboardUI/DashboardView.swift
+++ DashboardUI/DashboardView.swift
@@ -12,72 +12,53 @@ struct DashboardView: View {
     
     var body: some View {
         
-        ZStack(alignment: .top) {
-            
-            RoundedRectangle(cornerRadius: 15.0)
-                .foregroundColor(.white)
-            
-            VStack(alignment: .leading) {
+        GeometryReader{ reader in
+            ZStack(alignment: .top) {
                 
-                HStack(alignment: .top) {
-                    VStack(alignment: .leading) {
-                        Text("$1,394")
-                            .font(.system(.title))
-                            .bold()
-                        Text("+10% compared to last week")
-                            .font(.system(.caption))
-                            .foregroundColor(.gray)
-                            .frame(width: 150.0)
-                    }
-                    Spacer()
-                    Text("Weekly")
-                        .font(.system(.body))
-                        .bold()
-                }
-                .padding([.top, .leading, .trailing], 25)
+                RoundedRectangle(cornerRadius: 15.0)
+                    .foregroundColor(.white)
+                
+                VStack(alignment: .leading) {
                     
-                    VStack() {
-                        GeometryReader {reader in
-                            ZStack {
-                                Rectangle()
-                                    .foregroundColor(.white)
-                                Path { path in
-                                    
-                                    path.move(to: CGPoint(x: 0, y: reader.size.height))
-                                    path.addLine(to: CGPoint(x: 0, y: (reader.size.height)))
-                                    
-                                    var count: CGFloat = 1.0
-                                    let dataPoints = data.dropFirst()
-
-                                    dataPoints.forEach{p in
-                                    let x = (reader.size.width / CGFloat(data.count)) * count
-                                        let y = (reader.size.height - p)
-
-                                        path.addLine(to:
-                                            CGPoint(x: x, y: y)
-                                        )
-                                        
-                                        count += 1
-                                    }
-                                }
-                                .stroke(lineWidth: 8)
-                                .fill (LinearGradient (
-                                    gradient: .init(colors: .init([gradientStart, gradientEnd])),
-                                    startPoint: .init(x: 0, y: 0),
-                                    endPoint: .init(x: 1.0, y: 0)
-                                ))
-                            }
+                    HStack(alignment: .top) {
+                        VStack(alignment: .leading) {
+                            Text("$1,394")
+                                .font(.system(.title))
+                                .bold()
+                            Text("+10% compared to last week")
+                                .font(.system(.caption))
+                                .foregroundColor(.gray)
+                                .frame(width: 150.0)
                         }
-                        HStack {
-                            
-                            ForEach(chartLabels, id: \.self) {label in
-                                Text(label)
-                                    .frame(maxWidth: .infinity)
-                            }
-                        }
-                        .padding()
-                        .frame(maxWidth: .infinity)
+                        Spacer()
+                        Text("Weekly")
+                            .font(.system(.body))
+                            .bold()
                     }
+                    .padding([.top, .leading, .trailing], 25)
+                        
+                        VStack() {
+                            GeometryReader {reader in
+                                ZStack {
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                    LineView(frame: CGRect(x: 0,
+                                                           y: 0,
+                                                           width: reader.size.width,
+                                                           height: reader.size.height))
+                                }
+                            }
+                            HStack {
+                                
+                                ForEach(chartLabels, id: \.self) {label in
+                                    Text(label)
+                                        .frame(maxWidth: .infinity)
+                                }
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                        }
+                }
             }
         }
     }
